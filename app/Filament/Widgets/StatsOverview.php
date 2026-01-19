18bab9d2a7d8a51f00ff7184b67174658a9a3f4d
Iydann <11231001@student.itk.ac.id>
2026-01-19 16:45:14 +0800

    feat: add description to stat overview

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -25,22 +25,27 @@ class StatsOverview extends StatsOverviewWidget
 
         return [
             Stat::make('Total Projects', $totalProjects . ' Projects')
+                ->description('All projects in the system')
                 ->icon('heroicon-o-rectangle-stack')
                 ->color('info'),
 
             Stat::make('Active Projects', $activeProjects . ' Projects')
+                ->description('Projects currently in progress')
                 ->icon('heroicon-o-briefcase')
                 ->color('success'),
 
             Stat::make('Total Clients', $totalClients . ' Clients')
+                ->description('All clients in the system')
                 ->icon('heroicon-o-users')
                 ->color('primary'),
 
             Stat::make('Total Contracted Value', 'IDR ' . number_format($totalContractedValue, 0, ',', '.'))
+                ->description('Total value of all contracts')
                 ->icon('heroicon-o-currency-dollar')
                 ->color('secondary'),
 
             Stat::make('Total Paid Invoices', 'IDR ' . number_format($totalPaidInvoices, 0, ',', '.'))
+                ->description('Total amount of paid invoices')
                 ->icon('heroicon-o-banknotes')
                 ->color('success'),
         ];
